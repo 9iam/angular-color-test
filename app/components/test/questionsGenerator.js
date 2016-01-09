48e5fccf0be9346f6cf43a2de6c40a74e3e1fcfc
9iam <dziyatdi@ya.ru>
2016-01-09 20:29:03 +0300

    First draft version with colors

--- app/components/test/questionsGenerator.js
+++ app/components/test/questionsGenerator.js
@@ -4,37 +4,79 @@ angular.module('myApp.questionsGenerator', [
 ])
 
 .factory('questionsGenerator', function() {
+    var shades = {
+        pink: {
+            pink: '#F69ACD',
+            rose: '#FC94AD',
+            fuscia: '#FC46AA',
+            punch: '#F15278',
+            blush: '#FEC5E5',
+            watermelon: '#FE7F9C',
+            flamingo: '#FDA4B8',
+            rouge: '#F26B8B',
+            salmon: '#FDAB9F',
+            coral: '#FE7D68',
+            peach: '#FB9483',
+            strawberry: '#FC4C4E',
+            rosewood: '#A04242',
+            lemonade: '#FBBBCB',
+            taffy: '#FA86C5',
+            bubblegum: '#FD5CA8',
+            'ballet slipper': '#F69ABF',
+            crepe: '#F2B8C6',
+            magenta: '#E11584',
+            'hot pink': '#E11584'
+        }
+    };
+    // info from http://digitalsynopsis.com/design/color-thesaurus-correct-names-of-shades/
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function getRandomShadeWhichIsNot(shades, exception) {
+        var keys = Object.keys(shades), randomKey;                 // <- probably not supported in some browsers
+        do {
+            randomKey = keys[getRandomInt(0, keys.length-1)];
+        }
+        while (randomKey == exception)
+        return randomKey;
+    }
+
+    function getColorQuestion(shades, baseShade, numVariants) {
+        var question = {
+            question: 'Which color is ' + baseShade + '?',
+            variants: []
+        };
+
+        for (var i=0; i < numVariants; i++) {
+            var randomShade = getRandomShadeWhichIsNot(shades, baseShade);
+            question.variants.push({
+                text: randomShade,
+                color: shades[randomShade],
+                isRight: false
+            });
+        }
+
+        var theVariantIndex = getRandomInt(0, numVariants-1);;
+        question.variants[theVariantIndex] = {
+            text: baseShade,
+            color: shades[baseShade],
+            isRight: true
+        };
+
+        return question;
+    }
+
     this.getQuestions = function() {
-        return [
-            {
-                number: 1,
-                question: 'Do you like me?',
-                variants: [
-                {
-                    text: 'Yes',
-                    isRight: true,
-                }, {
-                    text: 'No',
-                    isRight: false,
-                },  {
-                    text: 'Maybe',
-                    isRight: false,
-                }]
-            }, {
-                 number: 2,
-                 question: 'What do you prefer to drink today?',
-                 variants: [
-                 {
-                     text: 'Water',
-                     isRight: false,
-                 }, {
-                     text: 'Cola',
-                     isRight: false,
-                 },  {
-                     text: 'Whiskey',
-                     isRight: true,
-                 }]
-            }];
+        const numQuestions = 5, numVariants = 4;
+        var result = [];
+        var keys = Object.keys(shades.pink);
+        for (var j=0; j < numQuestions; j++) {
+            var key = keys[j%keys.length];
+            result.push(getColorQuestion(shades.pink, key, numVariants));
+        }
+        return result;
     }
 
     return this;
